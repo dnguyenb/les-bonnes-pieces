20af337ec2b0d63525fb71cb1ef5ecac37ff85c1
David Nguyen Blot <d.nguyenblot@gmail.com>
2025-03-13 13:21:34 +0100

    add api-http

---
+++ frontend/avis.js
@@ -0,0 +1,41 @@
+export function ajoutListenersAvis() {
+	const piecesElements = document.querySelectorAll('.fiches article button');
+
+	for (let i = 0; i < piecesElements.length; i++) {
+		piecesElements[i].addEventListener('click', async (event) => {
+			const id = event.target.dataset.id;
+			const avis = await fetch(`http://localhost:8081/pieces/${id}/avis`)
+				.then((res) => res.json())
+				.catch((error) => console.error('Erreur :', error));
+
+			const avisElement = document.createElement('p');
+			for (let i = 0; i < avis.length; i++) {
+				avisElement.innerHTML += `<b>${avis[i].utilisateur} :</b> <br /> ${avis[i].commentaire}<br>`;
+			}
+			piecesElements[i].appendChild(avisElement);
+		});
+	}
+}
+
+export function ajoutListenerEnvoyerAvis() {
+	const formulaireAvis = document.querySelector('.formulaire-avis');
+	formulaireAvis.addEventListener('submit', (event) => {
+		event.preventDefault();
+		// Création de l’objet du nouvel avis.
+		const avis = {
+			pieceId: parseInt(event.target.querySelector('[name=piece-id]').value),
+			utilisateur: event.target.querySelector('[name=utilisateur').value,
+			commentaire: event.target.querySelector('[name=commentaire]').value,
+			nbEtoiles: parseInt(event.target.querySelector('[name=nbEtoiles]').value),
+		};
+		// Création de la charge utile au format JSON
+		const chargeUtile = JSON.stringify(avis);
+
+		// Envoi de l’avis au serveur
+		fetch('http://localhost:8081/avis', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: chargeUtile,
+		});
+	});
+}
